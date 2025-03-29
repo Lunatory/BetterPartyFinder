fafdc71d5bd632fbeb84a3c37b508c30ebda299f
Infi <infiziert@protonmail.ch>
2025-03-29 10:46:33 +0100

    - Rework Restrictions tab

--- BetterPartyFinder/Windows/Main/MainWindow.Restrictions.cs
+++ BetterPartyFinder/Windows/Main/MainWindow.Restrictions.cs
@@ -1,4 +1,5 @@
 ﻿using Dalamud.Game.Gui.PartyFinder.Types;
+using Dalamud.Interface.Colors;
 using Dalamud.Interface.Utility.Raii;
 using ImGuiNET;
 
@@ -6,119 +7,80 @@ namespace BetterPartyFinder.Windows.Main;
 
 public partial class MainWindow
 {
+    private bool Save;
+
     private void DrawRestrictionsTab(ConfigurationFilter filter)
     {
-        var practice = filter[ObjectiveFlags.Practice];
-        if (ImGui.Checkbox("Practice", ref practice))
-        {
-            filter[ObjectiveFlags.Practice] = practice;
-            Plugin.Config.Save();
-        }
+        using var table = ImRaii.Table("CategoryTable", 2, ImGuiTableFlags.BordersInnerV);
+        if (!table.Success)
+            return;
 
-        var dutyCompletion = filter[ObjectiveFlags.DutyCompletion];
-        if (ImGui.Checkbox("Duty completion", ref dutyCompletion))
-        {
-            filter[ObjectiveFlags.DutyCompletion] = dutyCompletion;
-            Plugin.Config.Save();
-        }
+        Save = false;
 
-        var loot = filter[ObjectiveFlags.Loot];
-        if (ImGui.Checkbox("Loot", ref loot))
-        {
-            filter[ObjectiveFlags.Loot] = loot;
-            Plugin.Config.Save();
-        }
+        ImGui.TableSetupColumn("##Show");
+        ImGui.TableSetupColumn("##Hide");
 
+        ImGui.TableNextColumn();
+        Helper.TextColored(ImGuiColors.HealerGreen, "Show:");
         ImGui.Separator();
 
-        var noCondition = filter[ConditionFlags.None];
-        if (ImGui.Checkbox("No duty completion requirement", ref noCondition))
-        {
-            filter[ConditionFlags.None] = noCondition;
-            Plugin.Config.Save();
-        }
-
-        var dutyIncomplete = filter[ConditionFlags.DutyIncomplete];
-        if (ImGui.Checkbox("Duty incomplete", ref dutyIncomplete))
-        {
-            filter[ConditionFlags.DutyIncomplete] = dutyIncomplete;
-            Plugin.Config.Save();
-        }
-
-        var dutyComplete = filter[ConditionFlags.DutyComplete];
-        if (ImGui.Checkbox("Duty complete", ref dutyComplete))
-        {
-            filter[ConditionFlags.DutyComplete] = dutyComplete;
-            Plugin.Config.Save();
-        }
-
-        var weeklyRewardUnclaimed = filter[ConditionFlags.DutyCompleteWeeklyRewardUnclaimed];
-        if (ImGui.Checkbox("Duty complete (weekly reward unclaimed)", ref weeklyRewardUnclaimed))
-        {
-            filter[ConditionFlags.DutyCompleteWeeklyRewardUnclaimed] = weeklyRewardUnclaimed;
-            Plugin.Config.Save();
-        }
-
+        ImGui.TableNextColumn();
+        Helper.TextColored(ImGuiColors.ParsedOrange, "Hide:");
         ImGui.Separator();
 
-        var undersized = filter[DutyFinderSettingsFlags.UndersizedParty];
-        if (ImGui.Checkbox("Undersized party", ref undersized))
-        {
-            filter[DutyFinderSettingsFlags.UndersizedParty] = undersized;
+        filter[ObjectiveFlags.Practice] = DrawRestrictionEntry("Practice", filter[ObjectiveFlags.Practice]);
+        filter[ObjectiveFlags.DutyCompletion] = DrawRestrictionEntry("Duty Completion", filter[ObjectiveFlags.DutyCompletion]);
+        filter[ObjectiveFlags.Loot] = DrawRestrictionEntry("Loot", filter[ObjectiveFlags.Loot]);
+
+        DrawSeparator();
+
+        filter[ConditionFlags.None] = DrawRestrictionEntry("No Completion Requirement", filter[ConditionFlags.None]);
+        filter[ConditionFlags.DutyIncomplete] = DrawRestrictionEntry("Duty Incomplete", filter[ConditionFlags.DutyIncomplete]);
+        filter[ConditionFlags.DutyComplete] = DrawRestrictionEntry("Duty Complete", filter[ConditionFlags.DutyComplete]);
+        filter[ConditionFlags.DutyCompleteWeeklyRewardUnclaimed] = DrawRestrictionEntry("Weekly Reward Unclaimed", filter[ConditionFlags.DutyCompleteWeeklyRewardUnclaimed]);
+
+        DrawSeparator();
+
+        filter[DutyFinderSettingsFlags.UndersizedParty] = DrawRestrictionEntry("Undersized Party", filter[DutyFinderSettingsFlags.UndersizedParty]);
+        filter[DutyFinderSettingsFlags.MinimumItemLevel] = DrawRestrictionEntry("Minimum Item Level", filter[DutyFinderSettingsFlags.MinimumItemLevel]);
+        filter[DutyFinderSettingsFlags.SilenceEcho] = DrawRestrictionEntry("Silence Echo", filter[DutyFinderSettingsFlags.SilenceEcho]);
+
+        DrawSeparator();
+
+        filter[LootRuleFlags.GreedOnly] = DrawRestrictionEntry("Greed Only", filter[LootRuleFlags.GreedOnly]);
+        filter[LootRuleFlags.Lootmaster] = DrawRestrictionEntry("Lootmaster", filter[LootRuleFlags.Lootmaster]);
+
+        DrawSeparator();
+
+        filter[SearchAreaFlags.DataCentre] = DrawRestrictionEntry("Data Centre Parties", filter[SearchAreaFlags.DataCentre]);
+        filter[SearchAreaFlags.World] = DrawRestrictionEntry("World-Local Parties", filter[SearchAreaFlags.World]);
+        filter[SearchAreaFlags.OnePlayerPerJob] = DrawRestrictionEntry("One Player Per Job", filter[SearchAreaFlags.OnePlayerPerJob]);
+
+        if (Save)
             Plugin.Config.Save();
+    }
+
+    private bool DrawRestrictionEntry(string name, bool state)
+    {
+        ImGui.TableNextRow();
+        ImGui.TableSetColumnIndex(state ? 0 : 1);
+
+        if (ImGui.Selectable(name))
+        {
+            state = !state;
+            Save = true;
         }
 
-        var minItemLevel = filter[DutyFinderSettingsFlags.MinimumItemLevel];
-        if (ImGui.Checkbox("Minimum item level", ref minItemLevel))
-        {
-            filter[DutyFinderSettingsFlags.MinimumItemLevel] = minItemLevel;
-            Plugin.Config.Save();
-        }
+        return state;
+    }
 
-        var silenceEcho = filter[DutyFinderSettingsFlags.SilenceEcho];
-        if (ImGui.Checkbox("Silence Echo", ref silenceEcho))
-        {
-            filter[DutyFinderSettingsFlags.SilenceEcho] = silenceEcho;
-            Plugin.Config.Save();
-        }
+    private void DrawSeparator()
+    {
+        ImGui.TableNextRow();
 
+        ImGui.TableNextColumn();
         ImGui.Separator();
-
-        var greedOnly = filter[LootRuleFlags.GreedOnly];
-        if (ImGui.Checkbox("Greed only", ref greedOnly))
-        {
-            filter[LootRuleFlags.GreedOnly] = greedOnly;
-            Plugin.Config.Save();
-        }
-
-        var lootmaster = filter[LootRuleFlags.Lootmaster];
-        if (ImGui.Checkbox("Lootmaster", ref lootmaster))
-        {
-            filter[LootRuleFlags.Lootmaster] = lootmaster;
-            Plugin.Config.Save();
-        }
-
+        ImGui.TableNextColumn();
         ImGui.Separator();
-
-        var dataCentre = filter[SearchAreaFlags.DataCentre];
-        if (ImGui.Checkbox("Data centre parties", ref dataCentre))
-        {
-            filter[SearchAreaFlags.DataCentre] = dataCentre;
-            Plugin.Config.Save();
-        }
-
-        var world = filter[SearchAreaFlags.World];
-        if (ImGui.Checkbox("World-local parties", ref world))
-        {
-            filter[SearchAreaFlags.World] = world;
-            Plugin.Config.Save();
-        }
-
-        var onePlayerPer = filter[SearchAreaFlags.OnePlayerPerJob];
-        if (ImGui.Checkbox("One player per job", ref onePlayerPer))
-        {
-            filter[SearchAreaFlags.OnePlayerPerJob] = onePlayerPer;
-            Plugin.Config.Save();
-        }
     }
 }
